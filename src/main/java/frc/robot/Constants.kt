bc8bf5d4d4db6f50fec41997d0e9dce4435da921
DanielRocksUrMom <108989218+DanielRocksUrMom@users.noreply.github.com>
2023-10-24 16:37:35 -0400

    Motor ID update

--- src/main/java/frc/robot/Constants.kt
+++ src/main/java/frc/robot/Constants.kt
@@ -12,12 +12,41 @@ import frc.chargers.controls.feedforward.AngularMotorFF
 import frc.chargers.controls.feedforward.Gravity
 import frc.chargers.controls.feedforward.LinearMotorFF
 import frc.chargers.controls.pid.PIDConstants
+import frc.chargers.hardware.motorcontrol.rev.neoSparkMax
+import frc.chargers.hardware.sensors.encoders.absolute.ChargerCANcoder
 import frc.chargers.hardware.swerve.SwerveControl
+import frc.chargers.hardware.swerve.sparkMaxSwerveMotors
+import frc.chargers.hardware.swerve.swerveCANcoders
 
 
 const val IS_REPLAY: Boolean = false
 
 object DriveConstants{
+
+    val turnMotors = sparkMaxSwerveMotors(
+        topLeft = neoSparkMax(29),
+        topRight = neoSparkMax(31),
+        bottomLeft = neoSparkMax(22),
+        bottomRight = neoSparkMax(5)
+    )
+
+    // TBD
+    val encoders = swerveCANcoders(
+        topLeft = ChargerCANcoder(0),
+        topRight = ChargerCANcoder(0),
+        bottomLeft = ChargerCANcoder(0),
+        bottomRight = ChargerCANcoder(0),
+        useAbsoluteSensor = true
+    )
+
+    val driveMotors = sparkMaxSwerveMotors(
+        topLeft = neoSparkMax(10),
+        topRight = neoSparkMax(16),
+        bottomLeft = neoSparkMax(30),
+        bottomRight = neoSparkMax(3)
+    )
+
+
 
     private val simTurnFF = LinearMotorFF(0.11.volts,0.26,0.0, distanceUnit = meters, gravity = Gravity.None)
         .convertToAngular(MK4i.TURN_GEAR_RATIO,4.inches)
@@ -29,13 +58,13 @@ object DriveConstants{
     )
 
     val realControlScheme = SwerveControl.PIDFirstOrder(
-        turnPIDConstants = PIDConstants(40.0,0.0,0.1),
+        turnPIDConstants = PIDConstants(10.0,0.0,0.0),
         drivePIDConstants = PIDConstants(0.1,0.0,0.0),
         driveFF = AngularMotorFF(0.11697.volts,0.133420,0.0, angleUnit = radians),
     )
 
-    val trackWidth = 29.inches
-    val wheelBase = 29.inches
+    val trackWidth = 32.5.inches
+    val wheelBase = 32.5.inches
 }
 
 
--- src/main/java/frc/robot/subsystems/Drive.kt
+++ src/main/java/frc/robot/subsystems/Drive.kt
@@ -4,37 +4,17 @@ package frc.robot.subsystems
 import edu.wpi.first.math.system.plant.DCMotor
 import edu.wpi.first.wpilibj.RobotBase
 import frc.chargers.constants.MK4i
-import frc.chargers.hardware.motorcontrol.rev.neoSparkMax
-import frc.chargers.hardware.sensors.encoders.absolute.ChargerCANcoder
 import frc.chargers.hardware.subsystems.drivetrain.EncoderHolonomicDrivetrain
 import frc.chargers.hardware.subsystems.drivetrain.realEncoderHolonomicDrivetrain
 import frc.chargers.hardware.subsystems.drivetrain.simEncoderHolonomicDrivetrain
-import frc.chargers.hardware.swerve.sparkMaxSwerveMotors
-import frc.chargers.hardware.swerve.swerveCANcoders
 import frc.robot.DriveConstants
 
 val Drivetrain: EncoderHolonomicDrivetrain by lazy{
     if (RobotBase.isReal()){
         realEncoderHolonomicDrivetrain(
-            turnMotors = sparkMaxSwerveMotors(
-                topLeft = neoSparkMax(0),
-                topRight = neoSparkMax(1),
-                bottomLeft = neoSparkMax(0),
-                bottomRight = neoSparkMax(0)
-            ),
-            turnEncoders = swerveCANcoders(
-                topLeft = ChargerCANcoder(0),
-                topRight = ChargerCANcoder(0),
-                bottomLeft = ChargerCANcoder(0),
-                bottomRight = ChargerCANcoder(0),
-                useAbsoluteSensor = true
-            ),
-            driveMotors = sparkMaxSwerveMotors(
-                topLeft = neoSparkMax(0),
-                topRight = neoSparkMax(1),
-                bottomLeft = neoSparkMax(0),
-                bottomRight = neoSparkMax(0)
-            ),
+            turnMotors = DriveConstants.turnMotors,
+            turnEncoders = DriveConstants.encoders,
+            driveMotors = DriveConstants.driveMotors,
             controlScheme = DriveConstants.realControlScheme,
             driveGearRatio = MK4i.GEAR_RATIO_L2,
             turnGearRatio = MK4i.TURN_GEAR_RATIO,
